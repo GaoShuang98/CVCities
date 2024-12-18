56cdc989bc06f32e3ae71c404f13d7dff8744571
爽hgs <huanggaoshuang123@163.com>
2024-12-18 11:35:01 +0800

    update __init__.py

--- cvcities_base/backbones/__init__.py
+++ cvcities_base/backbones/__init__.py
@@ -1,6 +1,4 @@
 from .efficientnet import EfficientNet
 from .resnet import ResNet
 from .swin import Swin
-from .DINOv2 import DinoV2
-from .DINOv2_new import DinoV2_
 from .DINOv2_self import DinoV2_self
